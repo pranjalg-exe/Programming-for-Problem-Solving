d0dc6c49df1132594327b4930f452d5afe5d0f72
Pranjal Gupta <pranjal.gupta.1586@gmail.com>
2025-04-10 20:41:20 +0530

    Update Leap_year.c.c to Leap_year.c

--- PPS/Operators/Leap_year.c
+++ PPS/Operators/Leap_year.c
@@ -5,6 +5,15 @@ int main(){
     int a;
     printf("Enter an year: ");
     scanf("%d",&a);
+        (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) 
+        ? printf("%d is a Leap Year.\n", year) 
+        : printf("%d is Not a Leap Year.\n", year);
+    return 0;
+
+}
+
+/*Using loops*/
+
     if (a%100==0){
         if(a%400==0){
             printf("It is a leap year");
@@ -21,4 +30,3 @@ int main(){
             printf("It is not a leap year");
         }
     }
-}
